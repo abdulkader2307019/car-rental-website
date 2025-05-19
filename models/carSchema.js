1ac4c882f6ec206eae94d2d22f342826d15dc4f9
abdulkader2307019 <abdulkader2307019@miuegypt.edu.eg>
2025-05-20 00:55:33 +0300

    setup ejs and handle some routing
    connect with mongo db and setup ejs

---
+++ models/carSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// define the Schema (the structure of the car)
+const carSchema = new Schema({
+  name: String,
+  brand: String,
+  type: String, // e.g., SUV, sedan
+  image: String,
+  pricePerDay: Number,
+  availability: Boolean,
+  location: String,
+  specs: { seats: Number, fuel: String, transmission: String }
+}, { timestamps: true });
+
+// Create a model based on that schema
+const Car = mongoose.model("car", carSchema);
+
+// export the model
+module.exports = Car;
