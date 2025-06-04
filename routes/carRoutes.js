adad41cc8f3f04c4d2e743a630fa1bf8b95952f9
Omarehab194 <omar2306195@miuegypt.edu.eg>
2025-06-04 19:23:33 +0300

    public : view cars
    router to view cars
    car controllers

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const carController = require('../controllers/carController');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
 
-router.post('/api/cars',protect,isAdmin,carController.addCar);
-router.put('/api/cars/:id',protect,isAdmin,carController.editCar);
-router.delete('/api/cars/:id',protect,isAdmin,carController.deleteCar);
+router.post('../api/cars',protect,isAdmin,carController.addCar);
+router.put('../api/cars/:id',protect,isAdmin,carController.editCar);
+router.delete('../api/cars/:id',protect,isAdmin,carController.deleteCar);
+router.get('../api/cars/:id',protect,carController.getCars);
 module.exports = router;
